0e43340ee9df4cc1edafcd9d8e5be5ff50b4cee7
Kod Planet <65843836+KodPlanet@users.noreply.github.com>
2021-03-20 19:00:24 +0300

    Add files via upload

---
+++ lib/services/auth.dart
@@ -0,0 +1,74 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class Auth {
+  final _firebaseAuth = FirebaseAuth.instance;
+
+  Future<User?> signInAnonymously() async {
+    try {
+      final UserCredential userCredentials =
+          await _firebaseAuth.signInAnonymously();
+      return userCredentials.user;
+    } catch (_) {
+      rethrow;
+    }
+  }
+
+  Future<User?> createUserWithEmailAndPassword(
+      String email, String password) async {
+    UserCredential userCredentials;
+    try {
+      userCredentials = await _firebaseAuth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      return userCredentials.user;
+    } on FirebaseAuthException catch (e) {
+      print(e.code);
+      print(e.message);
+      rethrow;
+    }
+  }
+
+  Future<User?> signInWithEmailAndPassword(
+      String email, String password) async {
+    final userCredentials = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+    return userCredentials.user;
+  }
+
+  Future<void> sendPasswordResetEmail(String email) async {
+    await _firebaseAuth.sendPasswordResetEmail(email: email);
+  }
+
+  Future<User?> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    if (googleUser != null) {
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+
+      // Create a new credential
+      final OAuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      // Once signed in, return the UserCredential
+      UserCredential? userCredential =
+          await _firebaseAuth.signInWithCredential(credential);
+      return userCredential.user;
+    } else {
+      return null;
+    }
+  }
+
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+    await GoogleSignIn().signOut();
+  }
+
+  Stream<User?>? authStatus() {
+    return _firebaseAuth.authStateChanges();
+  }
+}
---
+++ lib/views/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import '../services/auth.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              icon: Icon(Icons.logout),
+              onPressed: () async {
+                Provider.of<Auth>(context, listen: false).signOut();
+                print('logout tıklandı');
+              },
+            )
+          ],
+        ),
+        body: Center(child: Container(child: Text('HOME PAGE'))));
+  }
+}
